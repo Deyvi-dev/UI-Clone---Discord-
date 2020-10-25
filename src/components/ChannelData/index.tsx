f4be428e087f551e5049ff692391ac2bfa7eb883
root <deyvisson.margalhaes@gmail.com>
2020-10-24 23:49:52 -0300

    primeiro commit

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Dev-deyvi"
+            date="24/10/2020"
+            content="testandoo!"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Dev-deyvi"
+          date="21/06/2020"
+          content={
+            <>
+              <Mention>@deyvi</Mention>, vamos programar galera,saia do jogo!
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
